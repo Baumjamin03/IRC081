22b17fe3a6624aa2f7bf493e4caa355da37c2563
Benjamin Mitrovic <132056064+Baumjamin03@users.noreply.github.com>
2025-01-10 10:43:40 +0100

    lets see

--- Hardware_Control/P3Interface.py
+++ Hardware_Control/P3Interface.py
@@ -186,6 +186,7 @@ class P3(metaclass=abc.ABCMeta):
                 cmd = pkg_rcv[self.POSITION_CMD]
                 pid = struct.unpack(">H", pkg_rcv[self.POSITION_PID: self.POSITION_PID + 2])[0]
                 read_data = pkg_rcv[self.POSITION_DATA: -2]
+                data = []
                 if read_data:
                     data = self.data_callback(cmd, pid, read_data)
                 print("check 4")
