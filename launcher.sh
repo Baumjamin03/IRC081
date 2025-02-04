f2bb9b3ef61a00fa255d1580a2554d3790f6f4b7
Benjamin Mitrovic <132056064+Baumjamin03@users.noreply.github.com>
2025-02-04 16:24:13 +0100

    print

--- launcher.sh
+++ launcher.sh
@@ -5,4 +5,4 @@ export PYTHONPATH="${PYTHONPATH}:/usr/lib/python3/dist-packages"
 
 cd /home/rpiirc3/IRC081
 source venv/bin/activate
-python3 -m debugpy --listen 0.0.0.0:5678 --wait-for-client Interface.py
+python3 Interface.py
