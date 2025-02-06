66d94a064f283681df5ddf376754155b1833b4c5
Benjamin Mitrovic <132056064+Baumjamin03@users.noreply.github.com>
2025-02-06 11:17:38 +0100

    transfer

--- Interface.py
+++ Interface.py
@@ -328,9 +328,9 @@ class App(ctk.CTk):
             case 200:  # P3_PID_PRODUCTION_NUMBER
                 return tuple(ord(char) for char in self.irc081.getSerialNumber())
             case 201:  # GAUGE STATE
-                ai = self.irc081.get_digital_outputs()
+                di = self.irc081.get_digital_outputs()
 
-                return tuple(struct.pack('BB', 0, ai))
+                return tuple(struct.pack('BB', di, 0))
             case 207:  # P3_PID_SERIAL_NUMBER
                 """
                 """
